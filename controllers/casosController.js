a66bed6773d9225573620dd6af52f78ab1019507
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-08-09 19:39:02 -0300

    fix: correcao chamada de search

--- controllers/casosController.js
+++ controllers/casosController.js
@@ -68,7 +68,7 @@ async function searchCasos(req, res) {
         return res.status(404).json({ message: "Parâmetro de pesquisa 'q' não encontrado" });
     }
 
-    const searchedCasos = await casosRepository.searchByKeyword(search);
+    const searchedCasos = await casosRepository.search(search);
 
     if (!searchedCasos || searchedCasos.length === 0) {
         return res.status(404).json({
