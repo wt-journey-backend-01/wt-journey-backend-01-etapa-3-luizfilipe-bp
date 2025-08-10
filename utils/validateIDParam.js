070f4f2ff4309854f1e0000d4f53359c206c4320
Luiz Filipe Bartelega Penha <luizfpenha16@gmail.com>
2025-08-09 21:04:02 -0300

    fix: id validation

--- utils/validateIDParam.js
+++ utils/validateIDParam.js
@@ -1,6 +1,6 @@
 function validateIDParam(req, res, next) {
     const id = req.params.id;
-    if (!Number.isInteger(Number(id))) {
+    if (!/^\d+$/.test(id)) {
         return res.status(404).json({ message: 'O parâmetro ID deve ser um número inteiro' });
     }
     next();
